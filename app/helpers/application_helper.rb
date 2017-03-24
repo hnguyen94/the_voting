b116c8e22a686f9e2c2fb46ff5de0a957280319b
Hoang Nguyen <hoang.nguyen@xing.com>
2017-03-24 20:18:41 +0100

    make view cleaner and extract logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
 module ApplicationHelper
+  def show_user_name
+    # NOTE: Research for current_user&.name
+    # current_user.name if current_user
+    current_user&.name
+  end
+
+  def show_login_button
+    return link_to('Logout', signout_path) if current_user
+    link_to('Sign in with Facebook', '/auth/facebook')
+  end
 end
