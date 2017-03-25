5e02b61eb832d20887b50c7948021e8e7d2c30c3
Hoang Nguyen <hoang.nguyen@xing.com>
2017-03-25 16:08:47 +0100

    remove ugly url #_+_

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// Test for the ugliness.
+if (window.location.hash == '#_=_'){
+  // Check if the browser supports history.replaceState.
+  if (history.replaceState) {
+    // Keep the exact URL up to the hash.
+    var cleanHref = window.location.href.split('#')[0];
+    // Replace the URL in the address bar without messing with the back button.
+    history.replaceState(null, null, cleanHref);
+
+  } else {
+    // Well, you're on an old browser, we can get rid of the _=_ but not the #.
+    window.location.hash = '';
+  }
+}
