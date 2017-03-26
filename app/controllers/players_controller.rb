b0ac0a830884173d0f9b402f7786799436b02c9a
Hoang Nguyen <hoang.nguyen@xing.com>
2017-03-26 14:28:00 +0200

    Add games show and update players_controlelr

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
 class PlayersController < ApplicationController
+  before_filter :find_game_params, only: [:create, :destroy]
+
   def index
     @players = Player.all
   end
 
   def create
-    @game = Game.find(params[:game_id])
     @player = @game.players.create(user_id: current_user.id)
     redirect_to game_path(@game)
   end
 
   def destroy
-    @game = Game.find(params[:game_id])
-    @player = @game.players.create(user_id: current_user.id)
+    @player = Player.find_by!(user_id: params[:id])
     @player.destroy
     redirect_to game_path(@game)
   end
@@ -22,8 +23,12 @@ class PlayersController < ApplicationController
   end
 
   private
-  
+
   def player_params
     params.require(:player).permit(:user_id)
+  end
+
+  def find_game_params
+    @game = Game.find(params[:game_id])
   end
 end
