d440d0e7e9cf81c405a0405de6fb5e73e90e41f2
Hoang Nguyen <hoang.nguyen@xing.com>
2017-03-26 14:28:11 +0200

    change image size to square

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1818884588436079', '34cbe847fbee90e7c2ff319391f1dbc2',
            scope: 'public_profile',
-           image_size: 'large'
+           image_size: 'square'
 end
