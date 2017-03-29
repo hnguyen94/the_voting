dc7c775a22cba0e2ce3db3317e1b22ed2ae3c5a4
Hoang Nguyen <hoang.nguyen@xing.com>
2017-03-29 08:54:45 +0200

    Refactor application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,11 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      nil
+    end
   end
 
   helper_method :current_user
