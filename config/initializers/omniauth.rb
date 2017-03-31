910964b4dc44c0c8d5c55289f994782653d4a7cd
Hoang Nguyen <hoang.nguyen@xing.com>
2017-03-31 19:50:22 +0200

    Add figaro

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1818884588436079', '34cbe847fbee90e7c2ff319391f1dbc2',
+  provider :facebook, ENV['app_id'], ENV['secret_id'],
            scope: 'public_profile',
            image_size: 'square'
 end
