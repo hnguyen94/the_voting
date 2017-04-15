0682944b3c39effa753ab5861077e48b71f7c41e
Hoang Nguyen <hoang.nguyen@xing.com>
2017-04-15 14:08:24 +0200

    Clean up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
     flash[:notice] = 'Successfully logged in'
+
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
     flash[:alert] = 'Successfully logged out'
+
     redirect_to root_path
   end
 end
