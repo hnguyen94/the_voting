994f5f0e861c94f7f392d6cac6285a475ca4283c
Hoang Nguyen <hoang.nguyen@xing.com>
2017-04-15 17:50:31 +0200

    Add login without password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,22 @@
 
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
-    session[:user_id] = user.id
-    flash[:notice] = 'Successfully logged in'
+    user = if omniauth?
+             User.from_omniauth(env['omniauth.auth'])
+           else
+             User.find_by(name: params[:session][:name])
+           end
 
-    redirect_to root_path
+    if user
+      session[:user_id] = user.id
+      flash[:notice] = 'Successfully logged in'
+
+      return redirect_to root_path
+    end
+
+    flash[:notice] = 'Invalid name. Please Sign up'
+
+    redirect_to signup_path
   end
 
   def destroy
@@ -14,5 +25,11 @@ class SessionsController < ApplicationController
     flash[:alert] = 'Successfully logged out'
 
     redirect_to root_path
+  end
+
+  private
+
+  def omniauth?
+    !request.env['omniauth.auth'].nil?
   end
 end
