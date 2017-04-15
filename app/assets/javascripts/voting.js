bd686ceb6ccc3cc88bfa715aee159a2df701613f
MiBr7 <michi_brandt@t-online.de>
2017-04-15 15:57:59 +0200

    dynamic ajax request for voting

---
+++ app/assets/javascripts/voting.js
@@ -0,0 +1,28 @@
+
+setUpEventListener()
+
+function setUpEventListener(){
+  document.querySelector('.table').addEventListener('click', function(event){
+    document.querySelectorAll('.table tr td').forEach(function(element){
+      if(element.id === event.target.id){
+        makeAjaxRequest('PUT', path, element, replaceTableBy)
+      }
+    })
+  })
+}
+
+function makeAjaxRequest(type, path, element, callbackFunc){
+  var xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       callbackFunc(xhttp.response)
+    }
+};
+  xhttp.open(type, path, true)
+  xhttp.setRequestHeader()
+  xhttp.send()
+}
+
+function replaceTableBy(response){
+  document.querySelector('.table').innerHTML = response
+}
