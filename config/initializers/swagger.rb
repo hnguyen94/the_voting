0267447ee8606d9e17d44c36b7a90832f63b1324
Hoang Nguyen <hoang.nguyen@xing.com>
2017-08-08 12:32:19 +0200

    Fix app_url in swagger

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc'
-GrapeSwaggerRails.options.app_url  = 'http://thevoting-1.app.preview.ams2.xing.com/'
+GrapeSwaggerRails.options.app_url  = 'http://thevoting-1.app.preview.ams2.xing.com'
